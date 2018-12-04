315548aad0a20e507c20f83b367373a6e200ce11
Baili Deng <rayman0411@gmail.com>
2018-12-04 03:20:44 +0000

    Finished setting up topology

--- src/topology.py
+++ src/topology.py
@@ -14,8 +14,30 @@ from mininet.cli import CLI
 class Topo0 (Topo):
 	def build(self):
 		switch = []
+		host = []
+		#Create hosts h1~h6
+		for i in range(6):
+			host.append(self.addHost('i%s'%(i+1)))
+		#Create switches s1~s9
 		for i in range(9):
-			switch.append(self.addSwitch('s'+string(i)))
+			switch.append(self.addSwitch('s%s'%(i+1)))
+		#Since all the links have different statistics, generate them manually
+		#Python arrays start from 0, the index in array is one smaller than actual index
+		#Example: host[0] is h1
+		self.addLink( host[0], switch[0], bw=10, delay = '50us', loss = 12)	
+		self.addLink( host[1], switch[1], bw=5, delay = '2ms', loss = 3)	
+		self.addLink( host[2], switch[2], bw=7, delay = '63us', loss = 9)	
+		self.addLink( host[3], switch[3], bw=12, delay = '40us', loss = 14)	
+		self.addLink( host[4], switch[4], bw=15, delay = '30us', loss = 18)	
+		self.addLink( host[5], switch[5], bw=3, delay = '5ms', loss = 2)	
+		self.addLink( switch[0], switch[6], bw=23, delay = '1ms', loss = 8)	
+		self.addLink( switch[1], switch[6], bw=18, delay = '2ms', loss = 9)	
+		self.addLink( switch[2], switch[6], bw=15, delay = '3ms', loss = 5)	
+		self.addLink( switch[3], switch[7], bw=19, delay = '80us', loss = 7)	
+		self.addLink( switch[4], switch[7], bw=30, delay = '95us', loss = 2)	
+		self.addLink( switch[5], switch[7], bw=20, delay = '60us', loss = 6)	
+		self.addLink( switch[6], switch[8], bw=40, delay = '5ms', loss = 2)	
+		self.addLink( switch[7], switch[8], bw=50, delay = '4ms', loss = 3)	
 	
 
 #Create and test network
@@ -23,7 +45,6 @@ class Topo0 (Topo):
 def Test():
 	# Create topology based on topo0.png
 	topo = Topo0()
-	'''
 	# Create network
 	net = Mininet(topo)
 	# Start network
@@ -37,7 +58,6 @@ def Test():
 	net.pingALL()
 	#Don't stop, enter CLI mode
 	CLI(net)
-	'''
 	
 #Main
 
